1720b1690f434faf5845e17b19a56cdf933c5fc1
yonetaku0519 <yonezawa051319@gmail.com>
2023-12-16 10:42:39 +0900

    chore: Learn about functions

--- Typescript-practice/src/index.ts
+++ Typescript-practice/src/index.ts
@@ -1,9 +1,14 @@
-function log(message: string, userId?: string){
-    let time = new Date().toLocaleTimeString()
-    console.log(time,message,userId || 'Not signed in')
+// // 危険な書き方（n、totalともにanyであると型推論してしまう）
+// function sumVariadic(): number {
+//     return Array
+//         .from(arguments)
+//         .reduce((total, n) => total + n,0)
+// }
+// // エラーとなる「0 個の引数が必要ですが、4 個指定されました」
+// console.log(sumVariadic(1,2,3,4))
+
+// 安全な書き方（レストパラメーター）
+function sumVariadicSafe(...numbers: number[]): number {
+    return numbers.reduce((total, n) => total + n, 0)
 }
-
-log('User signed in', 'A0001')
-
-// 第二引数を使用しない
-log('Page loaded')
+console.log(sumVariadicSafe(1,2,3,4))
